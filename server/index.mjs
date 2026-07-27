e3d39298b248f0e1a43bc0e2e700402fde4fd99f
Alisson Teixeira <alisson.confweb@gmail.com>
2026-07-27 12:44:22 -0300

    Serve favicon with image content type

--- server/index.mjs
+++ server/index.mjs
@@ -1744,6 +1744,8 @@ function contentTypeFor(filePath) {
     ".css": "text/css; charset=utf-8",
     ".html": "text/html; charset=utf-8",
     ".ico": "image/x-icon",
+    ".jpeg": "image/jpeg",
+    ".jpg": "image/jpeg",
     ".js": "text/javascript; charset=utf-8",
     ".json": "application/json; charset=utf-8",
     ".png": "image/png",
